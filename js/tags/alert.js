fcb106820bd20d85c15556abf6781389f32f1f63
Matteo Leoni <leoni.matteo@gmail.com>
2016-05-09 16:09:42 +0200

    alert

---
+++ js/tags/alert.js
@@ -0,0 +1,25 @@
+riot.tag2('alert', '<div class="{classes()}" if="{!hidden}"><button if="{opts.dismissable}" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> {opts.message} </div>', 'alert .alert,[riot-tag="alert"] .alert,[data-is="alert"] .alert{ position: fixed; right: 20px; top: 20px; z-index: 10000; }', '', function(opts) {
+    this.mixin('SETTINGS')
+    var tag = this;
+
+    this.classes = function () {
+      var class = 'alert ';
+      if(opts.type === 'success')
+       class +="alert-success";
+      if(opts.type === 'danger')
+        class +="alert-danger";
+      if(opts.type === 'warning')
+        class +="alert-warning";
+      if(opts.dismissable)
+        class +=" alert-dismissable";
+      return class;
+    }
+
+    if(!opts.dismissable) {
+      setTimeout(function(){
+        tag.hidden = true;
+        tag.update();
+      },tag.settings.alert.timeout)
+    }
+
+});
